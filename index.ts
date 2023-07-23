a075e04d51a20ab35356ebecb45d7ef3937f7699
Mykola Grybyk <mykola.grybyk@gmail.com>
2023-07-23 10:33:09 +0200

    wip

--- index.ts
+++ index.ts
@@ -1,15 +1,30 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import io from '@actions/io'
+
+const baseDir = 'html-trend-report-action'
+const getBranchName = (gitRef: string) => gitRef.replace('refs/heads/', '')
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet')
-    console.log(`Hello ${nameToGreet}!`)
+    // vars
+    const reportId = core.getInput('report_id')
+    const branchName = getBranchName(github.context.ref)
+    const reportBaseDir = `${baseDir}/${branchName}/${reportId}`
+    const reportDir = `${reportBaseDir}/${github.context.runNumber}`
+
+    // log
+    console.table({ reportId, branchName, reportBaseDir, reportDir })
+    // context
+    const toLog = { ...github.context } as Record<string, unknown>
+    delete toLog.payload
+    console.log('toLog', toLog)
+
+    // action
+    await io.mkdirP(`${baseDir}/${branchName}/${reportId}/${github.context.runNumber}`)
+
+    // leftovers
     const time = new Date().toTimeString()
     core.setOutput('time', time)
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
 } catch (error) {
     core.setFailed(error.message)
 }
